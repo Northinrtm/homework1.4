5472a6cd0cec59969e2e6822005d4b49dd705d3e
northinrtm <artsdurden@gmail.com>
2022-08-11 19:58:08 +0300

    task1

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        int a = 1;
+        while(a <= 10) {
+            System.out.print(a);
+            System.out.print(" ");
+            a++;
+        }
+        System.out.println();
+        for(int i = 10;i != 0; i--){
+            System.out.print(i);
+            System.out.print(" ");
+        }
     }
 }
